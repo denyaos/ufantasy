daf2bbc2184132d3c13dffcf1ce5e5e6394dae1b
denys solohub <Denys_Solohub@epam.com>
2019-03-01 11:01:22 +0200

    Google authentication added

--- src/main/java/com/ufantasy/Controller.java
+++ src/main/java/com/ufantasy/Controller.java
@@ -1,12 +1,14 @@
 package com.ufantasy;
 
-import java.util.concurrent.atomic.AtomicLong;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.security.Principal;
+import java.util.concurrent.atomic.AtomicLong;
+
 @RestController
-public class GreetingController {
+public class Controller {
 
     private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
@@ -15,5 +17,10 @@ public class GreetingController {
     public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
         return new Greeting(counter.incrementAndGet(),
                             String.format(template, name));
+    }
+
+    @RequestMapping(value = "/user")
+    public Principal user(Principal principal) {
+        return principal;
     }
 }
